3150ccc205ab15516cf721cb9e04e477288d6c2a
mrizkyprtma <mrizkypratama1500@gmail.com>
2022-03-29 13:00:06 +0700

    menambahkan index.html,about.html,contact.html

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan </h1>";
+        echo "Nama saya Rizky
+        saya tinggal di jakarta timur
+        olahraga yang saya sukai adalah bulutangkis";
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title> latihan 1</tittle>
+</head>
+
+    <body>
+        Halo kawan.. Yuk kita belajar web programming..!!<br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari pemodelan dengan methode penjumlahan yaitu 
+        <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    </body>
+</html>
